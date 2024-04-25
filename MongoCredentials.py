1859257d77b3d361a365a802c65fe3efcb1b2103
mtkatsaros <60672119+mtkatsaros@users.noreply.github.com>
2024-04-25 14:57:12 -0700

    Revert "Documented MongoCredentials.py"
    This reverts commit 3f78532ab983269b82294f1d98ca3ff355f05095.

--- MongoCredentials.py
+++ MongoCredentials.py
@@ -1,9 +1,4 @@
-'''
-This file is meant to be edited to insert mongo credentials. "MongoCredentials.py" is in the .gitignore.
-To make your connection work, fill the information with your actual mongo connection info. It will be referenced in Utilities.py as:
-mongodb+srv://{username}:{password}@{project}.{hash}.mongodb.net/?retryWrites=true&w=majority
-'''
-password: str = 'password'
+password: str = "password"
 username: str = 'username'
 project: str = 'project'
 hash_name: str = 'hashname'
