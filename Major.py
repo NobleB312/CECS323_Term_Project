71f9c3d0f18318747307b2f505b91f16c4b72dd8
lbzfran <lbscout1@gmail.com>
2024-04-27 22:52:44 -0700

    major, student, studentmajor added
    filled out the classes of major, student, studentmajor

---
+++ Major.py
@@ -0,0 +1,32 @@
+from mongoengine import *
+from datetime import datetime
+#from Department import Department
+
+class Major(Document):
+    """
+    
+    """
+    majorName = StringField(db_field='major_name',max_length=20,required=True)
+    majorDescription = StringField(db_field='major_description',max_length=800,required=True)
+    
+    ## FIXME: reference to Department ##
+    department = ReferenceField(Department, required=True, reverse_delete_rule=DENY)
+
+    meta = { 'collection': 'majors',
+             'indexes': [
+                {'unique': True, 'fields': ['majorName'], 'name': 'majors_uk_1'},
+                {'unique': True, 'fields': ['majorDescription'], 'name': 'majors_uk_2'}
+             ]}
+
+    def __init__(self, majorName: str, majorDescription: str, *args, **values):
+        super().__init__(*args, **values)
+        self.majorName = majorName
+        self.majorDescription = majorDescription
+
+    def __str__(self):
+        results = f'Major: {self.majorName}: {self.description}'
+
+## FIXME ##
+    def get_department(self):
+        """:return:        identity of the major's department."""
+        return self.department
---
+++ Student.py
@@ -0,0 +1,56 @@
+from mongoengine import *
+from datetime import datetime
+from Major import Major
+from StudentMajor import StudentMajor
+
+class Student(Document):
+    """"""
+    firstName = StringField(db_field='first_name', max_length=40, required=True)
+    lastName = StringField(db_field='last_name', max_length=40, required=True)
+    eMail = StringField(db_field='email', max_length=100, required=True)
+    studentMajor = EmbeddedDocumentListField(StudentMajor,db_field='student_major')
+    
+    ## FIXME: reference to Enrollment ##
+    
+
+    meta = {'collection': 'students',
+            'indexes': [
+                {'unique': True, 'fields': ['lastName', 'firstName'], 'name': 'students_uk_1'},
+                {'unique': True, 'fields': ['eMail'], 'name': 'students_uk_2'}
+                ]}
+
+
+    def __init__(self, firstName: str, lastName: str, eMail: str, *args, **values):
+
+        super().__init__(*args,**values)
+        self.firstName = firstName
+        self.lastName = lastName
+        self.eMail = eMail
+
+    def __str__(self):
+        results = f'Student: {lastName}, {firstName} Email: {eMail}'
+        for declaredmajor in self.studentMajor:
+            results += '\n\t' + f"declared Major: {declaredmajor.major} on {declaredmajor.declarationDate}"
+        return results
+
+    def add_major(self, new_major: StudentMajor):
+
+        if self.studentMajor:
+            # adding constraints:
+            # student cannot declare major they previously declared.
+            # student can declare the same major at different time intervals; the assumption is that they dropped the major and returned, which realistically, is a very small chance of occurring multiple times.
+            current_major = self.studentMajor[-1]
+            if current_major == new_major:
+                raise ValueError('Student is already in that major.')
+            if current_major.declarationDate >= new_major.declarationDate:
+                raise ValueError('The major cannot be declared before the latest major declared.')
+            if new_major.declarationDate > datetime.utcnow():
+                raise ValueError('The major cannot be declared in the future.')
+            self.studentMajor.append(new_major)
+        else:
+            self.studentMajor = [new_major]
+
+    def get_current_major(self) -> Major:
+        if self.studentMajor:
+            return self.studentMajor[-1].major
+        return None
