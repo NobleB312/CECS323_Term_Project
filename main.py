0fa5ef799e16dcfdacfa4a1efb9ed97f59d80683
Vincent Pierlot <80808294+NobleB312@users.noreply.github.com>
2024-05-02 15:27:00 -0700

    Update main.py
    Added add_department()
    Need to finish add_major()

--- main.py
+++ main.py
@@ -74,6 +74,36 @@ def select_student_major():
 def select_enrollment():
     return select_general(Enrollment)
 
+def add_department():
+    """
+    Create a new Department instance.
+    """
+    success = False
+    while not success:
+        try:
+            department_name = prompt_for_input('Enter the department name: ')
+            department_abbreviation = prompt_for_input('Enter the department abbreviation: ')
+            department_chair_name = prompt_for_input('Enter the department chair name: ')
+            department_building = prompt_for_input('Enter the department building: ')
+            department_office = int(prompt_for_input('Enter the department office number: '))
+            department_description = prompt_for_input('Enter the department description: ')
+
+            new_department = Department(
+                departmentName=department_name,
+                departmentAbbreviation=department_abbreviation,
+                departmentChairName=department_chair_name,
+                departmentBuilding=department_building,
+                departmentOffice=department_office,
+                departmentDescription=department_description
+            )
+            
+            new_department.save()
+            print(f'Successfully added department: {new_department.departmentName}')
+            success = True
+
+        except Exception as e:
+            print('An error occurred: ', Utilities.print_exception(e))
+
 
 def add_course():
     """
@@ -109,6 +139,33 @@ def add_course():
 
         except NotUniqueError as nue:
             print('Error: The course violates one or more uniqueness constraints. Please enter unique values.')
+
+
+# def add_major():
+#     """
+#     Create a new Major instance.
+#     """
+#     success = False
+#     while not success:
+#         try:
+#             major_name = prompt_for_input('Enter the major name (up to 20 characters): ')
+#             major_description = prompt_for_input('Enter the major description (up to 800 characters): ')
+            
+#             department_name = prompt_for_input('Enter the department name: ')
+#             department = Department.objects.get(departmentName=department_name)
+
+#             new_major = Major(
+#                 majorName=major_name,
+#                 majorDescription=major_description,
+#                 department=department
+#             )
+            
+#             new_major.save()
+#             print(f'Successfully added major: {new_major.majorName}')
+#             success = True
+
+#         except Exception as e:
+#             print(f'An error occurred: {e}')
 
 
 #function to delete a course
